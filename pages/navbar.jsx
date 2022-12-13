7d9dfb578c1689aac956858f464f8054764afb87
Isaac <isaacsun0813@gmail.com>
2022-12-12 23:28:07 -0500

    vercer images

--- pages/navbar.jsx
+++ pages/navbar.jsx
@@ -30,7 +30,7 @@ const Navbar=()=>{
                         : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/'>
-                    <Image src="../is_portfolio/public/photos/logo.JPEG" alt="logo" width='125' height='50'/>
+                    <Image src="../public/photos/logo.JPEG" alt="logo" width='125' height='50'/>
                 </Link>
                 <div>
                     <ul className='hidden md:flex'>
